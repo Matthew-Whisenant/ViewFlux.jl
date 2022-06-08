2763b6844a3ecb4bbc72e18bccd30e316033f03b
Matthew Whisenant <mwhisena@vols.utk.edu>
2022-06-08 02:19:08 -0400

    Fixed observations with plotting

--- src/mlplots.jl
+++ src/mlplots.jl
@@ -12,116 +12,129 @@
     Create easy to use plot function.
 
 """
-function mlplot(mlopts, dataopts, netdata)
+function mlplot(mlopts, dataopts, netdata, loss)
 
     # Get number of networks
     num_nets = length(mlopts.model)
 
-    # Initialize observables
-    VR = Vector{Real}
+    # Create tuple to hold vectors observables
     obs =
-        (
-            # Predicted output
-            yp_model=[Observable{VR}([]) for i = 1:num_nets],
+        let VF = Vector{Float64}, VI = Vector{Int}
+            (
+                # Initialize observables
+                idx=Observable{Int}(1),
 
-            # Epoch list for slider
-            epoch_vec=Observable{VR}([]),
+                # Predicted output
+                yp_model=[Observable{Vector{VF}}([]) for i = 1:num_nets],
 
-            # Loss list
-            loss_vec_train=[Observable{Vector{VR}}([]) for i = 1:num_nets],
-            loss_vec_test=[Observable{Vector{VR}}([]) for i = 1:num_nets]
-        )
+                # Epoch list for slider
+                epoch_vec=Observable{VI}([0]),
 
-    # Marker shapes
-    truemarker = :diamond
-    trainmarker = :circle
-    testmarker = :rect
+                # Loss list
+                loss_vec_train=[Observable{VF}([]) for i = 1:num_nets],
+                loss_vec_test=[Observable{VF}([]) for i = 1:num_nets]
+            )
+        end
 
     # Set theme
-    set_theme!(theme_black())
-    update_theme!(linewidth=3, markersize=15)
+    with_theme(theme_black(), linewidth=3, markersize=15) do
 
-    # Create figure
-    fig = Figure(resolution=(800, 1000))
+        # Marker shapes
+        truemarker = :diamond
+        trainmarker = :circle
+        testmarker = :rect
 
-    # Axes and labels
-    ax = (
-        Axis(
-            fig[1, 1],
-            xlabel="Input X",
-            ylabel="Output Y"
-        ),
-        Axis(
-            fig[2, 1],
-            xlabel="Epoch",
-            ylabel="Loss",
-            yscale=log10
+        # Create figure
+        fig = Figure(resolution=(800, 1000))
+
+        # Axes and labels
+        ax = (
+            Axis(
+                fig[1, 1],
+                xlabel="Input X",
+                ylabel="Output Y"
+            ),
+            Axis(
+                fig[2, 1],
+                xlabel="Epoch",
+                ylabel="Loss",
+                yscale=log10
+            )
         )
-    )
 
-    # Make top plot bigger
-    rowsize!(fig.layout, 1, Relative(2 / 3))
+        # Make top plot bigger
+        rowsize!(fig.layout, 1, Relative(2 / 3))
 
-    # Epoch slider to control predicted values
-    sl = Slider(fig[3, 1], range=@lift(0:$(obs.epoch_vec)[end]), startvalue=0)
-
-    # Plot actual data
-    scatterlines!(
-        fig[1, 1],
-        dataopts.x, dataopts.y;
-        marker=truemarker,
-        label="Sample Function"
-    )
-
-    for i = 1:num_nets
-
-        # Train and test input
-        xp_train = (reduce(vcat, netdata.train[i].data[1])|>cpu)[:, end]
-        xp_test = (reduce(vcat, netdata.test[i].data[1])|>cpu)[:, end]
-
-        # Predicted train and test output
-        yp_train = reduce(vcat,
-            [mlopts.model[i](xi)[:, end] for
-             xi in netdata.train[i].data[1]]) |> cpu
-        yp_test = reduce(vcat,
-            [mlopts.model[i](xi)[:, end] for
-             xi in netdata.test[i].data[1]]) |> cpu
-
-        yp_model = Observable([[yp_train; yp_test]])
-
-        markerops = [
-            fill(trainmarker, length(yp_train))
-            fill(testmarker, length(yp_test))
-        ]
-
-        # Predicted output
+        # Plot actual data
         scatterlines!(
             fig[1, 1],
-            reduce(vcat, [xp_train; xp_test]),
-            reduce(vcat, yp_model[]);
-            marker=markerops,
-            label="Net " * string(i)
+            dataopts.x, dataopts.y;
+            marker=truemarker,
+            label="Sample Function"
         )
 
-        reset!(mlopts.model[i])
+        for i = 1:num_nets
 
-        # Show training and testing loss
-        lines!(
-            fig[2, 1],
-            epoch_vec, loss_vec_train[i];
-            label="Net " * string(i) * " training"
-        )
+            # Train and test input
+            xp_train = (reduce(vcat, netdata.train[i].data[1])|>cpu)[:, end]
+            xp_test = (reduce(vcat, netdata.test[i].data[1])|>cpu)[:, end]
 
-        lines!(
-            fig[2, 1],
-            epoch_vec, loss_vec_test[i];
-            linestyle=:dash,
-            label="Net " * string(i) * " testing")
+            xp = [xp_train; xp_test]
+
+            # Predicted train and test output
+            yp_train = reduce(vcat,
+                [mlopts.model[i](xi)[:, end] for
+                 xi in netdata.train[i].data[1]]) |> cpu
+            yp_test = reduce(vcat,
+                [mlopts.model[i](xi)[:, end] for
+                 xi in netdata.test[i].data[1]]) |> cpu
+
+            push!(obs.yp_model[i][], [yp_train; yp_test])
+
+            # Append network losses
+            reset!(mlopts.model[i])
+            push!(obs.loss_vec_train[i][], loss(netdata.train[i].data..., mlopts.model[i]))
+            push!(obs.loss_vec_test[i][], loss(netdata.test[i].data..., mlopts.model[i]))
+            reset!(mlopts.model[i])
+
+            # Predicted output
+            markerops = [
+                fill(trainmarker, length(yp_train))
+                fill(testmarker, length(yp_test))
+            ]
+
+            # Predicted output
+            scatterlines!(
+                fig[1, 1],
+                reduce(vcat, xp),
+                @lift(reduce(vcat, $(obs.yp_model[i])[$(obs.idx)]));
+                marker=markerops,
+                label="Net " * string(i)
+            )
+
+            # Show training and testing loss
+            lines!(
+                fig[2, 1],
+                obs.epoch_vec, obs.loss_vec_train[i];
+                label="Net " * string(i) * " training"
+            )
+
+            lines!(
+                fig[2, 1],
+                obs.epoch_vec, obs.loss_vec_test[i];
+                linestyle=:dash,
+                label="Net " * string(i) * " testing")
+
+        end
+
+        # Epoch slider to control predicted values
+        sl = Slider(fig[3, 1], range=@lift(0:$(obs.epoch_vec)[end]), startvalue=0)
+        connect!(obs.idx, sl.selected_index)
+
+        Legend(fig[0, 1], fig.content[1], orientation=:horizontal, tellwidth=false, tellheight=true, framevisible=false)
+
+        return fig, obs, sl
 
     end
-
-    Legend(fig[0, 1], fig.content[1], orientation=:horizontal, tellwidth=false, tellheight=true, framevisible=false)
-
-    return fig, yp_model, sl
 
 end
