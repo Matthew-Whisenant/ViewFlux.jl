d9c16c6df5e267f580fae4b41744beaaa4cdb2fc
Matthew Whisenant <mwhisena@vols.utk.edu>
2022-06-08 02:19:24 -0400

    Run now works with observations

--- src/mlrun.jl
+++ src/mlrun.jl
@@ -22,17 +22,17 @@ function mlrun(mlopts, dataopts, plotopts)
     loss(x, y, model) = sum(mse(model(xi)[:, end], yi) for (xi, yi) in zip(x, y)) / length(x)
 
     # Set up first frame of gif
-    fig, yp_model, sl = mlplot(mlopts, dataopts, netdata)
+    fig, obs, sl = mlplot(mlopts, dataopts, netdata, loss)
     display(fig)
 
     # Now do training repeatedly
     @showprogress 0.1 "Training..." for epoch in 1:plotopts.num_epochs
 
         # Update epoch count
-        push!(epoch_vec[], epoch)
+        push!(obs.epoch_vec[], epoch)
 
         # For each network
-        for i = 1:num_nets
+        for i = 1:length(mlopts.model)
 
             reset!(mlopts.model[i])
 
@@ -51,28 +51,29 @@ function mlrun(mlopts, dataopts, plotopts)
 
             # Append network losses
             reset!(mlopts.model[i])
-            loss_vec_train[i].val = push!(loss_vec_train[i][], loss(netdata.train[i].data..., mlopts.model[i]))
-            loss_vec_test[i].val = push!(loss_vec_test[i][], loss(netdata.test[i].data..., mlopts.model[i]))
+            push!(obs.loss_vec_train[i][], loss(netdata.train[i].data..., mlopts.model[i]))
+            push!(obs.loss_vec_test[i][], loss(netdata.test[i].data..., mlopts.model[i]))
+            reset!(mlopts.model[i])
 
             # Append predicted model output
-            reset!(mlopts.model[i])
             yp_train = reduce(vcat,
                 [mlopts.model[i](xi)[:, end] for
                  xi in netdata.train[i].data[1]]) |> cpu
             yp_test = reduce(vcat,
-                [mlopts[i].model[i](xi)[:, end] for
+                [mlopts.model[i](xi)[:, end] for
                  xi in netdata.test[i].data[1]]) |> cpu
-            yp_model.val = push!(yp_model[], [yp_train; yp_test])
+            push!(obs.yp_model[i][], [yp_train; yp_test])
 
-            # Save next frame of gif
-            if mod(epoch, plotopts.epoch_plot) == 0
+        end
 
-                sl.range = 0:epoch_vec[][end]
-                autolimits!.(fig.content[1:2])
-                sl.selected_index = epoch - 1
-                sleep(0.001)
+        # Save next frame of gif
+        if mod(epoch, plotopts.epoch_plot) == 0
 
-            end
+            autolimits!.(fig.content[1:2])
+            notify(obs.epoch_vec)
+            obs.idx[] = epoch
+            sl.selected_index[] = obs.idx[]
+            sleep(1 / plotopts.fps)
 
         end
 
